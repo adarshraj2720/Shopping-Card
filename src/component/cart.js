c24bb891040564adfa1eae7a0edc949ecbec141e
Adarsh Raj <adarshraj2721@gmail.com>
2022-08-05 20:31:00 +0530

    shopping card

--- src/component/cart.js
+++ src/component/cart.js
@@ -1,102 +1,222 @@
 import React from "react";
-import data from '../data.json'
+import { useState } from "react";
+// import data from '../data.json'
 
-class Cart extends React.Component {
-    constructor(props) {
-        super()
-        this.state = ({
-            istrue: true,
-            incdec:false
 
-        })
+
+//function component uses:-
+
+
+
+
+function Cart(props) {
+
+    let [display, setdisplay] = useState(false)
+    let [changeqty, setchangeqty] = useState(false)
+    let [cart, setcart] = useState([])
+
+    function handleclick() {
+        setdisplay(true);
+        setcart(props.info)
     }
-
-
-    handletrue = () => {
-        this.setState({
-            istrue: !this.state.istrue
-        })
+    function handledelete() {
+        setdisplay(false)
+        setchangeqty(false)
     }
-
-
-    handleremove = () => {
-        this.setState({
-            istrue: !this.state.istrue
-        })
-    }
-
-    handleInc = (event) => {
-        let id = event.target.id;
-        let singleProduct = data.products.filter((p) => p.id == id);
+    function handleInc(event) {
+        let id = event.target.id
+        let singleProduct = props.info.filter((p) => p.id == id);
         singleProduct[0].Qty = singleProduct[0].Qty + 1;
-        this.setState({
-          incdec: true,
-        });
-      };
-      handleDec = (event) => { 
+        setchangeqty(true)
+        setcart([...cart], singleProduct[0])
+    }
+    function handleDec(event) {
         let id = event.target.id;
-        let singleProduct = data.products.filter((p) => p.id == id);
-        if(singleProduct[0].Qty>1){
-        singleProduct[0].Qty = singleProduct[0].Qty - 1;
+        let singleProduct = props.info.filter((p) => p.id === id)
+        if (singleProduct[0].Qty > 1) {
+            singleProduct[0].Qty = singleProduct[0].Qty - 1;
         }
+        setcart([...cart], singleProduct[0])
+        setchangeqty(true)
+    }
+    function handlecheckout() {
+       
+        alert( [...new Set (cart)].reduce((acc,cv)=>{
+            acc =  acc + cv.price * cv.Qty
+            return acc
+        },0))
+    }
 
-        console.log(singleProduct[0].Qty);
-        this.setState({
-          incdec: true,
-        });
-      };
-    
 
-    render() {
-        return (
-            <>
+    return (
+        <>
+            {display === false ? (
                 <div >
                     <figure>
-                        <img className="bagicon" onClick={this.handletrue} src={'/static/bag-icon.png'} ></img>
+                        <img className="bagicon" onClick={handleclick} src={'/static/bag-icon.png'} ></img>
                     </figure>
-                    <small className="cartlength">{[... new Set(this.props.info)].length}</small>
+                    <small className="cartlength">{[... new Set(props.info)].length}</small>
                 </div>
 
-                {
-                    this.state.istrue ? ""
-                        :
-                        <div className="cartcategory">
-                            <div className="shoppingsummary">
-                                <p className="summary">Shopping Summary</p>
-                                <button onClick={this.handleremove}>❌</button>
-                            </div>
+            ) : (
+                <div className="cartcategory">
+                    <div className="shoppingsummary">
+                        <p className="summary">Shopping Summary</p>
+                        <button onClick={handledelete}>❌</button>
+                    </div>
 
-                            {
-                                [... new  Set(this.props.info)].map((a) => {
-                                    return (
-                                        <div className="cartlist">
-                                        <img className="cartproductimg" src={'/static/products/' + `${a.sku}` + '_1.jpg'}></img>
-                                            <p>{a.title}</p>
-                                            {/* <p>Quantity:1</p> */}
-                                            {
-                                                this.state.incdec===true?
-                                                <p>Quantity:{a.Qty}</p>:<p>Quantity:{a.Qty}</p>
-                                            }
-                                            <button id={a.id} onClick={this.handleInc}>+</button>
-                                            <button id={a.id} onClick={this.handleDec}>-</button>
-                                            <p>Price:{a.currencyFormat}{a.price}</p>
-                                        
-                                        </div>
-                                    )
-                                })
-                            }
-                            <div className="shoppingsummary">
-                                <p className="summary">SubTotal: { [... new  Set(this.props.info)].reduce((acc, cv) => {
-                                    acc = acc + cv.price *cv.Qty
-                                    return acc;
-                                }, 0)}</p>
-                            </div>
-                        </div>
-                }
-            </>
-        )
-    }
+                    {
+                        [... new Set(cart)].map((a) => {
+                            return (
+                                <div className="cartlist">
+                                    <img className="cartproductimg" src={'/static/products/' + `${a.sku}` + '_1.jpg'}></img>
+                                    <p>Title: {a.title}</p>
+                                    <p>Sizes: {a.availableSizes}</p>
+
+                                    {
+                                        changeqty === true ?
+                                            <p>Quantity:{a.Qty}</p>
+                                            :
+                                            <p>Quantity:{a.Qty}</p>
+                                    }
+                                    <button id={a.id} onClick={handleInc}>+</button>
+                                    <button id={a.id} onClick={handleDec}>-</button>
+                                    <p>Price:{a.currencyFormat}{a.price}</p>
+
+                                </div>
+                            )
+                        })}
+
+                    <div className="shoppingsummary">
+                        <p className="summary">SubTotal: {[... new Set(props.info)].reduce((acc, cv) => {
+                            acc = acc + cv.price * cv.Qty
+                            return acc;
+                        }, 0)}</p>
+                        <button onClick={handlecheckout} >
+
+                            
+                            Checkout</button>
+                    </div>
+                </div>
+            )}
+        </>
+    )
+
+
+
+
+
+
+
+
+
+
 
 }
+
+
+
+
+
+
+//class component uses:-
+
+// class Cart extends React.Component {
+//     constructor(props) {
+//         super()
+//         this.state = ({
+//             istrue: true,
+//             incdec:false
+
+//         })
+//     }
+
+
+//     handletrue = () => {
+//         this.setState({
+//             istrue: !this.state.istrue
+//         })
+//     }
+
+
+//     handleremove = () => {
+//         this.setState({
+//             istrue: !this.state.istrue
+//         })
+//     }
+
+//     handleInc = (event) => {
+//         let id = event.target.id;
+//         let singleProduct = data.products.filter((p) => p.id == id);
+//         singleProduct[0].Qty = singleProduct[0].Qty + 1;
+//         this.setState({
+//           incdec: true,
+//         });
+//       };
+//       handleDec = (event) => { 
+//         let id = event.target.id;
+//         let singleProduct = data.products.filter((p) => p.id == id);
+//         if(singleProduct[0].Qty>1){
+//         singleProduct[0].Qty = singleProduct[0].Qty - 1;
+//         }
+
+//         console.log(singleProduct[0].Qty);
+//         this.setState({
+//           incdec: true,
+//         });
+//       };
+
+
+//     render() {
+//         return (
+//             <>
+//                 <div >
+//                     <figure>
+//                         <img className="bagicon" onClick={this.handletrue} src={'/static/bag-icon.png'} ></img>
+//                     </figure>
+//                     <small className="cartlength">{[... new Set(this.props.info)].length}</small>
+//                 </div>
+
+//                 {
+//                     this.state.istrue ? ""
+//                         :
+//                         <div className="cartcategory">
+//                             <div className="shoppingsummary">
+//                                 <p className="summary">Shopping Summary</p>
+//                                 <button onClick={this.handleremove}>❌</button>
+//                             </div>
+
+//                             {
+//                                 [... new  Set(this.props.info)].map((a) => {
+//                                     return (
+//                                         <div className="cartlist">
+//                                         <img className="cartproductimg" src={'/static/products/' + `${a.sku}` + '_1.jpg'}></img>
+//                                             <p>{a.title}</p>
+//                                             {/* <p>Quantity:1</p> */}
+//                                             {
+//                                                 this.state.incdec===true?
+//                                                 <p>Quantity:{a.Qty}</p>:<p>Quantity:{a.Qty}</p>
+//                                             }
+//                                             <button id={a.id} onClick={this.handleInc}>+</button>
+//                                             <button id={a.id} onClick={this.handleDec}>-</button>
+//                                             <p>Price:{a.currencyFormat}{a.price}</p>
+
+//                                         </div>
+//                                     )
+//                                 })
+//                             }
+//                             <div className="shoppingsummary">
+//                                 <p className="summary">SubTotal: { [... new  Set(this.props.info)].reduce((acc, cv) => {
+//                                     acc = acc + cv.price *cv.Qty
+//                                     return acc;
+//                                 }, 0)}</p>
+//                             </div>
+//                         </div>
+//                 }
+//             </>
+//         )
+//     }
+
+// }
 
 export default Cart
--- src/component/products.js
+++ src/component/products.js
@@ -1,127 +1,256 @@
 import React from "react";
-import data from '../data.json'
+import { useState } from "react";
 
+import data from '../data.json'
 import Cart from "./cart";
 
-class Products extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = ({
-            products: data.products,
-            cart: true,
-            list: [],
-        })
-    }
-
-    handlesize = (event) => {
-        let size = event.target.innerText
-        let filtersize = data.products.filter((fsize) => fsize.availableSizes.includes(size))
-        this.setState({
-            products: filtersize,
-        })
-        console.log(filtersize)
-    }
-
-    handlecart = (event) => {
-        let id = event.target.id;
-        let prodctlist = data.products.filter((a) => a.id == id)
-        prodctlist[0].Qty=1
-        this.setState({
-            list: this.state.list.concat(prodctlist)
-        })
-        // console.log(this.state.list)
-    }
 
 
-    handlerange = (event) => {
+
+//function component uses:-
+
+function Products() {
+    let [allProduct, setallProduct] = useState(data.products);
+    let [isCart, setisCart] = useState([]);
+    let [isActive, setisActive] = useState("");
+
+    function rangefilter(event) {
         let range = event.target.value
-        let incorder = data.products.sort((a, b) => b.price - a.price)
-        let desorder = data.products.sort((a, b) => a.price - b.price)
+        console.log(range)
 
         if (range === "Highest to Lowest") {
-            this.setState(
-                { products: data.products.sort((a, b) => b.price - a.price) }
-            )
-        }
-        if (range === "Lowest to Highest") {
-            this.setState({
-                products: data.products.sort((a, b) => a.price - b.price)
-            })
+            let HightoLow = data.products.sort((p, q) => q.price - p.price)
+            console.log(HightoLow)
+            //allProduct = HightoLow
+             setallProduct(HightoLow)
         }
 
-        console.log(range)
+        if (range == "Lowest to Highest") {
+            let LowtoHigh = data.products.sort((p, q) => p.price - q.price)
+            console.log(LowtoHigh)
+            setallProduct(LowtoHigh)
+        }
     }
 
 
-    render() {
-        return (
+    function handlecart(event) {
+        let id = event.target.id;
+        let prodctlist = data.products.filter((a) => a.id == id)
+        prodctlist[0].Qty = 1
+        setisCart(isCart.concat(prodctlist))
+    }
 
-            <main>
-                <div className="cart">
-                    <Cart info={this.state.list} />
-                </div>
-                <div className="filter-btn">
-                    <div>
-                        <div className="size-btns">
-                            <div className="sizetitle">
-                                <p>Sizes:</p>
-                            </div>
-                            {
-                                ["S", "XS", "M", "X", "L", "XL", "XXL"].map((size) => {
-                                    return (
-                                        <>
-                                            <button className="size-btn" onClick={(event) => { this.handlesize(event) }}>{size}</button>
-                                        </>
-                                    )
-                                })
-                            }
-                        </div>
-                        <div className="filterrange">
-                            <p className="rangetitle">Order By:</p>
-                            <select onChange={(event) => { this.handlerange(event) }}  >
 
-                                <option>Select</option>
-                                <option value="Highest to Lowest">
-                                    Highest to Lowest
-                                </option>
-                                <option value="Lowest to Highest">
-                                    Lowest  to Highest
-                                </option>
-                            </select>
+    function handlesize(event) {
+        let size = event.target.innerText
+        console.log(size)
+        let filtersize = data.products.filter((fsize) => fsize.availableSizes.includes(size))
+    
+        setallProduct(filtersize)
+        setisActive(size)
+    }
+
+
+    return (
+
+        <main>
+            <div className="cart">
+                <Cart info={isCart} />
+            </div>
+            <div className="filter-btn">
+                <div>
+                    <div className="size-btns">
+                        <div className="sizetitle">
+                            <p>Sizes:</p>
                         </div>
+                        {
+                            ["S", "XS", "M", "X", "L", "XL", "XXL"].map((size) => {
+                                return (
+                                    <>
+                                        <button onClick={handlesize} className={isActive == size ? "btnactive size-btn" : "size-btn"} >{size}</button>
+                                    </>
+                                )
+                            })
+                        }
                     </div>
-
+                    <div className="filterrange">
+                        <p className="rangetitle">Order By:</p>
+                        <select onChange={rangefilter} >
+                            <option>Select</option>
+                            <option value="Highest to Lowest">
+                                Highest to Lowest
+                            </option>
+                            <option value="Lowest to Highest">
+                                Lowest  to Highest
+                            </option>
+                        </select>
+                    </div>
                 </div>
-                <div className="length">
-                    <p>{this.state.products.length} Products find</p>
-                </div>
 
-                <div className="product">
-   
+            </div>
+            <div className="length">
+                <p>{allProduct.length} Products find</p>
+            </div>
 
-                    {
+            <div className="product">
 
-                        this.state.products.map((product) => {
 
-                            return (
+                {
 
-                                <div className="products" id={product.id} >
+                    allProduct.map((product) => {
 
-                                    <figure>
-                                        <img src={'/static/products/' + `${product.sku}` + '_1.jpg'}></img>
-                                    </figure>
+                        return (
 
-                                    <p className="producttitle">{product.title}</p>
-                                    <p className="productprice">Price:{product.currencyFormat}{product.price}</p>
-                                    <button className="addcartbtn" id={product.id} onClick={(event) => { this.handlecart(event) }}>Add To Cart</button>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-            </main>
-        )
-    }
+                            <div className="products" id={product.id} >
+
+                                <figure>
+                                    <img src={'/static/products/' + `${product.sku}` + '_1.jpg'}></img>
+                                </figure>
+
+                                <p className="producttitle">{product.title}</p>
+                                <p className="productprice">Price:{product.currencyFormat}{product.price}</p>
+                                <button className="addcartbtn" id={product.id} onClick={handlecart}>Add To Cart</button>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </main>
+    )
 }
 
-export default Products
+
+
+
+
+
+
+
+
+
+
+
+//class function component uses:-
+
+// class Products extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = ({
+//             products: data.products,
+//             cart: true,
+//             list: [],
+//         })
+//     }
+
+//     handlesize = (event) => {
+//         let size = event.target.innerText
+//         let filtersize = data.products.filter((fsize) => fsize.availableSizes.includes(size))
+//         this.setState({
+//             products: filtersize,
+//         })
+//         console.log(filtersize)
+//     }
+
+//     handlecart = (event) => {
+//         let id = event.target.id;
+//         let prodctlist = data.products.filter((a) => a.id == id)
+//         prodctlist[0].Qty = 1
+//         this.setState({
+//             list: this.state.list.concat(prodctlist)
+//         })
+//         // console.log(this.state.list)
+//     }
+
+
+//     handlerange = (event) => {
+//         let range = event.target.value
+//         let incorder = data.products.sort((a, b) => b.price - a.price)
+//         let desorder = data.products.sort((a, b) => a.price - b.price)
+
+//         if (range === "Highest to Lowest") {
+//             this.setState(
+//                 { products: data.products.sort((a, b) => b.price - a.price) }
+//             )
+//         }
+//         if (range === "Lowest to Highest") {
+//             this.setState({
+//                 products: data.products.sort((a, b) => a.price - b.price)
+//             })
+//         }
+
+//         console.log(range)
+//     }
+
+
+//     render() {
+//         return (
+
+//             <main>
+//                 <div className="cart">
+//                     <Cart info={this.state.list} />
+//                 </div>
+//                 <div className="filter-btn">
+//                     <div>
+//                         <div className="size-btns">
+//                             <div className="sizetitle">
+//                                 <p>Sizes:</p>
+//                             </div>
+//                             {
+//                                 ["S", "XS", "M", "X", "L", "XL", "XXL"].map((size) => {
+//                                     return (
+//                                         <>
+//                                             <button className="size-btn" onClick={(event) => { this.handlesize(event) }}>{size}</button>
+//                                         </>
+//                                     )
+//                                 })
+//                             }
+//                         </div>
+//                         <div className="filterrange">
+//                             <p className="rangetitle">Order By:</p>
+//                             <select onChange={(event) => { this.handlerange(event) }}  >
+
+//                                 <option>Select</option>
+//                                 <option value="Highest to Lowest">
+//                                     Highest to Lowest
+//                                 </option>
+//                                 <option value="Lowest to Highest">
+//                                     Lowest  to Highest
+//                                 </option>
+//                             </select>
+//                         </div>
+//                     </div>
+
+//                 </div>
+//                 <div className="length">
+//                     <p>{this.state.products.length} Products find</p>
+//                 </div>
+
+//                 <div className="product">
+
+
+//                     {
+
+//                         this.state.products.map((product) => {
+
+//                             return (
+
+//                                 <div className="products" id={product.id} >
+
+//                                     <figure>
+//                                         <img src={'/static/products/' + `${product.sku}` + '_1.jpg'}></img>
+//                                     </figure>
+
+//                                     <p className="producttitle">{product.title}</p>
+//                                     <p className="productprice">Price:{product.currencyFormat}{product.price}</p>
+//                                     <button className="addcartbtn" id={product.id} onClick={(event) => { this.handlecart(event) }}>Add To Cart</button>
+//                                 </div>
+//                             )
+//                         })
+//                     }
+//                 </div>
+//             </main>
+//         )
+//     }
+// }
+
+export default Products;
